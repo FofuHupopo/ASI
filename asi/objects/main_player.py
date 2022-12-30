bd3a96e6a020aa08158dde99c9edc17946c81b11
ivch1717 <118736025+ivch1717@users.noreply.github.com>
2022-12-30 13:12:37 +0500

    база 2
    + метательное оружие

--- asi/objects/main_player.py
+++ asi/objects/main_player.py
@@ -4,20 +4,23 @@ import random
 from typing import Sequence
 
 from engine.objects import BaseSprite
+from asi.objects.throwing_arms import Arms
 
 
 class PlayerObject(BaseSprite):
     def init(self):
         self.load_image("player/creature.png")
+        self.weidht = self.image.get_width()
         self.rect.x = 500
         self.rect.y = 250
         self.speed_x = 0
         self.speed_y = 0
         self.time_y = 0
+        self.direction = 1
         self.fly = False
 
     def update(self):
-        self.rect.x -= self.speed_x
+        self.rect.x += self.speed_x
         if self.fly:
             self.rect.y -= self.speed_y
             self.speed_y -= self.time_y
@@ -28,11 +31,15 @@ class PlayerObject(BaseSprite):
         if event.type == pygame.KEYDOWN and keys[pygame.K_SPACE] and not self.fly:
             self.speed_y = 5
             self.fly = True
+        if event.type == pygame.KEYDOWN and keys[pygame.K_r]:
+            t = Arms(coords=[self.rect.x + max(0, self.weidht * self.direction), self.rect.y], direction=self.direction)
 
     def key_pressed_handler(self, pressed: Sequence[bool]):
         if pressed[pygame.K_a]:
-            self.speed_x = 3
+            self.direction = -1
+            self.speed_x = 3 * self.direction
         elif pressed[pygame.K_d]:
-            self.speed_x = -3
+            self.direction = 1
+            self.speed_x = 3 * self.direction
         else:
             self.speed_x = 0
