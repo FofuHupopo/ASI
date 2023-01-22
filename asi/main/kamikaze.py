9e23314d775f88ea0f66d11311d025110fcd73c1
iv_17 <ivch1717@gmail.com>
2023-01-22 16:43:19 +0500

    base enemy

--- asi/main/kamikaze.py
+++ asi/main/kamikaze.py
@@ -6,13 +6,13 @@ from dataclasses import dataclass
 from engine.objects import BaseSprite
 from engine.core import EngineEvent, EventTypes
 from engine.objects.sprite import SpriteTypes
+from engine.objects.enemy import BaseEnemy
 
 from .obstacle import Obstacle
 
 
-class Kamikaze(BaseSprite):
+class Kamikaze(BaseEnemy):
     def init(self, coords):
-        self.set_type(SpriteTypes.ENEMY)
         self.load_image("enemy/kamikadze.jpg")
         self.scale_image((100, 100))
 
@@ -32,76 +32,13 @@ class Kamikaze(BaseSprite):
         self.zone_y = self.rect.y - 250
         self.direction = 1
 
-    def agra(self):
-        return None
+        self.attack_radius_x = 100
+        self.attack_radius_x = 100
 
-    def find_zone(self):
-        self.flag_zone = True
-        self.rect.y += 50
-        for i in range(10):
-            if self.checking_touch_by_type(SpriteTypes.OBSTACLE):
-                self.zone_x1 -= 50
-                self.rect.x -= 50
-
-                self.rect.y -= 50
-                for j in range(1, 6):
-                    self.rect.y -= 50
-                    if self.checking_touch_by_type(SpriteTypes.OBSTACLE):
-                        self.zone_y = max(self.zone_y, self.rect.y + 50)
-                        break
-                self.rect.y = self.main_coordsy + 50
-            else:
-                self.rect.x = self.main_coordsx
-                break
-        for i in range(10):
-            if self.checking_touch_by_type(SpriteTypes.OBSTACLE):
-                self.zone_x2 += 50
-                self.rect.x += 50
-
-                self.rect.y -= 50
-                for j in range(1, 6):
-                    self.rect.y -= 50
-                    if self.checking_touch_by_type(SpriteTypes.OBSTACLE):
-                        self.zone_y = max(self.zone_y, self.rect.y + 50)
-                        break
-                self.rect.y = self.main_coordsy + 50
-            else:
-                break
-        self.rect.y = self.main_coordsy
-        self.rect.x = self.main_coordsx
-        self.zone_x1 = self.zone_x1 - self.rect.x
-        self.zone_x2 = self.zone_x2 - self.rect.x
-        self.zone_y = self.rect.y - self.zone_y
+        self.speed = 3
+        self.speed_agra = 5
 
     def attack(self):
         self.kill()
 
-    def update(self):
-        self.coords_player = self.find_sprites(SpriteTypes.PLAYER)[0].rect
-        if not self.flag_zone:
-            self.find_zone()
-        if self.coords_player.x >= self.zone_x1 + self.rect.x - self.relacetion_x and \
-                self.coords_player.x <= self.zone_x2 + self.rect.x - self.relacetion_x \
-                and self.coords_player.y >= - self.zone_y + self.rect.y and self.coords_player.y <= self.rect.y:
-            if abs(self.coords_player.x - self.rect.x) < 100 and abs(self.coords_player.y - self.rect.y < 100):
-                self.attack()
-            if self.coords_player.x > self.rect.x:
-                self.rect.x += 5
-                self.relacetion_x += 5
-                self.direction = 1
-            else:
-                self.rect.x -= 5
-                self.relacetion_x -= 5
-                self.direction = -1
-        elif self.direction == 1:
-            if self.relacetion_x < self.zone_x2 - self.width:
-                self.rect.x += 3
-                self.relacetion_x += 3
-            else:
-                self.direction = -1
-        else:
-            if self.relacetion_x > self.zone_x1 + self.width:
-                self.rect.x -= 3
-                self.relacetion_x -= 3
-            else:
-                self.direction = 1
+
---
+++ engine/objects/enemy.py
@@ -0,0 +1,81 @@
+import pygame
+import random
+
+from typing import Sequence
+from dataclasses import dataclass
+from engine.objects import BaseSprite
+from engine.core import EngineEvent, EventTypes
+from engine.objects.sprite import SpriteTypes
+
+class BaseEnemy(BaseSprite):
+    def init(self):
+        self.set_type(SpriteTypes.ENEMY)
+
+    def find_zone(self):
+        self.flag_zone = True
+        self.rect.y += 50
+        for i in range(10):
+            if self.checking_touch_by_type(SpriteTypes.OBSTACLE):
+                self.zone_x1 -= 50
+                self.rect.x -= 50
+
+                self.rect.y -= 50
+                for j in range(1, 6):
+                    self.rect.y -= 50
+                    if self.checking_touch_by_type(SpriteTypes.OBSTACLE):
+                        self.zone_y = max(self.zone_y, self.rect.y + 50)
+                        break
+                self.rect.y = self.main_coordsy + 50
+            else:
+                self.rect.x = self.main_coordsx
+                break
+        for i in range(10):
+            if self.checking_touch_by_type(SpriteTypes.OBSTACLE):
+                self.zone_x2 += 50
+                self.rect.x += 50
+
+                self.rect.y -= 50
+                for j in range(1, 6):
+                    self.rect.y -= 50
+                    if self.checking_touch_by_type(SpriteTypes.OBSTACLE):
+                        self.zone_y = max(self.zone_y, self.rect.y + 50)
+                        break
+                self.rect.y = self.main_coordsy + 50
+            else:
+                break
+        self.rect.y = self.main_coordsy
+        self.rect.x = self.main_coordsx
+        self.zone_x1 = self.zone_x1 - self.rect.x
+        self.zone_x2 = self.zone_x2 - self.rect.x
+        self.zone_y = self.rect.y - self.zone_y
+
+    def update(self):
+        self.coords_player = self.find_sprites(SpriteTypes.PLAYER)[0].rect
+        if not self.flag_zone:
+            self.find_zone()
+        elif self.coords_player.x >= self.zone_x1 + self.rect.x - self.relacetion_x and \
+                self.coords_player.x <= self.zone_x2 + self.rect.x - self.relacetion_x \
+                and self.coords_player.y >= - self.zone_y + self.rect.y and self.coords_player.y <= self.rect.y:
+            if abs(self.coords_player.x - self.rect.x) < 100 and abs(self.coords_player.y - self.rect.y < 100):
+                self.attack()
+            elif self.coords_player.x > self.rect.x:
+                self.rect.x += self.speed_agra
+                self.relacetion_x += self.speed_agra
+                self.direction = 1
+            else:
+                self.rect.x -= self.speed_agra
+                self.relacetion_x -= self.speed_agra
+                self.direction = -1
+        elif self.direction == 1:
+            if self.relacetion_x < self.zone_x2 - self.width:
+                self.rect.x += self.speed
+                self.relacetion_x += self.speed
+            else:
+                self.direction = -1
+        else:
+            if self.relacetion_x > self.zone_x1 + self.width:
+                self.rect.x -= self.speed
+                self.relacetion_x -= self.speed
+            else:
+                self.direction = 1
+
