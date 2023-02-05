8979d774b0f557dd4e5fb0b195636ba9a6e1beed
FofuHupopo <fofuhupopo@gmail.com>
2023-02-05 13:56:36 +0500

    asd

--- asi/main/trader.py
+++ asi/main/trader.py
@@ -3,13 +3,23 @@ import random
 
 from typing import Sequence
 
-from engine.objects import BaseSprite
+from engine.objects import BaseSprite, AnimatedSprite
 from engine.objects.sprite import SpriteTypes
 
 
-class Trader(BaseSprite):
+class Trader(AnimatedSprite):
     def init(self, coords):
-        self.load_image("npc/trader.png")
+        # self.load_image()
+        self.register_animations(
+            "npc/trader.png",
+            {
+                "test": [
+                    "npc/trader.png",
+                    "npc/profile_icon.png",
+                    "npc/asprite.bmp"
+                ]
+            }
+        )
         self.scale_image((80, 100))
         self.set_type(SpriteTypes.NPC)
         self.coords = coords
@@ -18,6 +28,14 @@ class Trader(BaseSprite):
         self.width = self.image.get_width()
         self.height = self.image.get_height()
 
+    def events_handler(self, event: pygame.event.Event):
+        pressed = pygame.key.get_pressed()
+        
+        if event.type == pygame.KEYDOWN and pressed[pygame.K_g]:
+            self.start_animation("test")
+            
+        if event.type == pygame.KEYDOWN and pressed[pygame.K_f]:
+            self.stop_animation()
 
     def buy(self):
         pass
